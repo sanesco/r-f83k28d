04cf3cadae28d075ab8159844bbe85587b2abb2f
sanesco <santiagoescobar11z@gmail.com>
2019-07-23 00:04:17 -0500

    Adds functionality to increases number

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        count: 0
+     }
+}
+
+
   render() {
+    console.log(this.state.count);
     return (
       <div>
-        <span className="value">1</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <button id="inc" onClick={this.addValue.bind(this)}>Incrementa</button>
       </div>
     );
   }
+
+    addValue(){
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
 }
 
 export default App;
